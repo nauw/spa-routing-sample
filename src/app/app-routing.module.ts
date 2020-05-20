c78b75600081b85382177de4986cfbbeb9e050e8
Arnaud Charlier <Arnaud>
2020-05-21 01:10:06 +0200

    Creating submodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {EncodingComponent} from './encoding/encoding.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'encoding',
+    component: EncodingComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
